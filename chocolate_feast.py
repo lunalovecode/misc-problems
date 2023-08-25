0e6b214009c241ac4306d7e5f7e9ada1ecfd11ff
lunalovecode <lunalovecode@gmail.com>
2023-08-25 15:27:43 +0800

    Add some problems solved for August 25

---
+++ chocolate_feast.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'chocolateFeast' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER c
+#  3. INTEGER m
+#
+
+def chocolateFeast(n, c, m):
+    num_eaten, wrappers = 0, 0
+    while n - c >= 0:
+        num_eaten += 1
+        wrappers += 1
+        n -= c
+        if wrappers == m:
+            num_eaten += 1
+            wrappers -= m - 1
+    
+    return num_eaten
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        c = int(first_multiple_input[1])
+
+        m = int(first_multiple_input[2])
+
+        result = chocolateFeast(n, c, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
