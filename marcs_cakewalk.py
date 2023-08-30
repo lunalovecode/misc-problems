01005f80c096ab95a7a4d22f219568478f62342f
lunalovecode <lunalovecode@gmail.com>
2023-08-30 18:54:03 +0800

    Add problems for August 30

---
+++ marcs_cakewalk.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marcsCakewalk' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY calorie as parameter.
+#
+
+def marcsCakewalk(calorie):
+    cupcakes = [*calorie]
+    cupcakes.sort(reverse=True)
+    i = 0
+    total_calories = 0
+    for c in cupcakes:
+        total_calories += (2 ** i) * c
+        i += 1
+    return total_calories
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    calorie = list(map(int, input().rstrip().split()))
+
+    result = marcsCakewalk(calorie)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
